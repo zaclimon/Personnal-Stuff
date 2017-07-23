4898841e61c1893f91d00115326b48ab5b226182
zaclimon <isaacpateau05@gmail.com>
2017-07-23 16:43:27 -0400

    Win10Hook.sh: Add dynamic binding for the onboard audio controller
    There was always an issue whilst using PulseAudio to redirect sound
    from the host. This issue was that it caused sound popping and
    HTML5 videos that were getting out of sync.
    
    Let's pass the entire chipset onboard audio to the VM to completely
    fix the issue even though the audio on the host is lost during
    that time.

--- scripts/Win10Hook.sh
+++ scripts/Win10Hook.sh
@@ -20,9 +20,15 @@ LIBVIRT_COMMAND="$2"
 VM_IP_ADDRESS="192.168.5.100"
 WLAN_INTERFACE="wlp6s0"
 PROXY_ARP_STATUS=$(arp -e | grep "Isaac-VM.localdomain" | grep -c "$WIFI_INTERFACE")
-USB_CONTROLLER_ID="0000:02:00.0"
-USB_CONTROLLER_DRIVER_XHCI=$(lspci -s $USB_CONTROLLER_ID -v | grep "Kernel driver" | grep -c "xhci_hcd")
-USB_CONTROLLER_DRIVER_VFIO_PCI=$(lspci -s $USB_CONTROLLER_ID -v | grep "Kernel driver" | grep -c "vfio-pci")
+
+# Variables for PCI-E devices
+USB_CONTROLLER_PCI_ID="0000:02:00.0"
+ONBOARD_AUDIO_DEVICE_ID="8086 8ca0"
+ONBOARD_AUDIO_PCI_ID="0000:00:1b.0"
+USB_CONTROLLER_DRIVER_XHCI=$(lspci -s $USB_CONTROLLER_PCI_ID -v | grep "Kernel driver" | grep -c "xhci_hcd")
+USB_CONTROLLER_DRIVER_VFIO_PCI=$(lspci -s $USB_CONTROLLER_PCI_ID -v | grep "Kernel driver" | grep -c "vfio-pci")
+ONBOARD_AUDIO_DRIVER_SND_HDA_INTEL=$(lspci -s $ONBOARD_AUDIO_PCI_ID -v | grep "Kernel driver" | grep -c "snd_hda_intel")
+ONBOARD_AUDIO_DRIVER_VFIO_PCI=$(lspci -s $ONBOARD_AUDIO_PCI_ID -v | grep "Kernel driver" | grep -c "vfio-pci")
 
 # Export stuff required for swapping screens when root
 export DISPLAY=:0
@@ -31,32 +37,50 @@ export XAUTHORITY=/home/isaac/.Xauthority
 if [ "$CURRENT_MACHINE" = "$VM_NAME" ] ; then
     if [ "$LIBVIRT_COMMAND" = "started" ] ; then
 
-        # Unbind the controller from the XHCI driver and bind it to the vfio-pci one.
-        if [ $USB_CONTROLLER_DRIVER_XHCI -eq 1 ] ; then
-            echo "$USB_CONTROLLER_ID > /sys/bus/pci/drivers/xhci_hcd/unbind"
-            echo "$USB_CONTROLLER_ID > /sys/bus/pci/drivers/vfio-pci/bind"
-        fi
-
         # Enable Proxy arp if not already
         if [ $PROXY_ARP_STATUS -eq 0 ] ; then
             arp -i "$WLAN_INTERFACE" -Ds "$VM_IP_ADDRESS" "$WLAN_INTERFACE" pub
         fi
 
-        # Start the Synergy server, change the default display to the secondary one and crank that volume!!
+        # Unbind the controller from the XHCI driver and bind it to the vfio-pci one.
+        if [ $USB_CONTROLLER_DRIVER_XHCI -eq 1 ] ; then
+            echo "$USB_CONTROLLER_PCI_ID > /sys/bus/pci/drivers/xhci_hcd/unbind"
+            echo "$USB_CONTROLLER_PCI_ID > /sys/bus/pci/drivers/vfio-pci/bind"
+        fi
+
+        # Do the same for the onboard audio controller. We need to register it's id first
+        # since it won't get recognized by vfio-pci. Note that we don't need to do this on
+        # the USB controller because it is in the same IOMMU group as the GPU's.
+        if [ $ONBOARD_AUDIO_DRIVER_SND_HDA_INTEL -eq 1 ] ; then
+            echo "$ONBOARD_AUDIO_PCI_ID" > /sys/bus/pci/drivers/snd_hda_intel/unbind
+            echo "$ONBOARD_AUDIO_PCI_ID" > /sys/bus/pci/drivers/vfio-pci/bind
+
+            if [ $? -eq 1 ] ; then
+                echo "$ONBOARD_AUDIO_DEVICE_ID" > /sys/bus/pci/drivers/vfio-pci/new_id
+                echo "$ONBOARD_AUDIO_PCI_ID" > /sys/bus/pci/drivers/vfio-pci/bind
+            fi
+
+        fi
+
+        # Start the Synergy server and change the default display to the secondary one.
         synergys
         xrandr --output VGA-1 --auto --output HDMI-1 --off
-        amixer set Master 75%
     elif [ "$LIBVIRT_COMMAND" = "stopped" ] ; then
 
         # Unbind the controller from the vfio-pci driver and bind it to the XHCI one.
         if [ $USB_CONTROLLER_DRIVER_VFIO_PCI -eq 1 ] ; then
-            echo "$USB_CONTROLLER_ID > /sys/bus/pci/drivers/vfio-pci/unbind"
-            echo "$USB_CONTROLLER_ID > /sys/bus/pci/drivers/xhci_hcd/bind"
+            echo "$USB_CONTROLLER_PCI_ID > /sys/bus/pci/drivers/vfio-pci/unbind"
+            echo "$USB_CONTROLLER_PCI_ID > /sys/bus/pci/drivers/xhci_hcd/bind"
         fi
 
-        # Stop Synergy, restore display status and let's become a bit more reasonable on the volume...
+        # Do the same for the onboard audio
+        if [ $ONBOARD_AUDIO_DRIVER_VFIO_PCI -eq 1 ] ; then
+            echo "$ONBOARD_AUDIO_PCI_ID" > /sys/bus/pci/drivers/vfio-pci/unbind
+            echo "$ONBOARD_AUDIO_PCI_ID" > /sys/bus/pci/drivers/snd_hda_intel/bind
+        fi
+
+        # Stop Synergy and restore display status
         pkill synergy
         xrandr --output HDMI-1 --auto --output VGA-1 --off
-        amixer set Master 25%
     fi
 fi
